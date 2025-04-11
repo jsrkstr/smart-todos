1d14bc859d452b0ccbeb9f8937a3462963c11a99
Sachin Saluja <sachin.saluja132@gmail.com>
2025-04-11 11:15:46 +0200

    Update page.tsx

--- apps/web/app/tasks/page.tsx
+++ apps/web/app/tasks/page.tsx
@@ -1,11 +1,14 @@
 import { AppLayout } from "@/components/layouts/app-layout"
 import { TasksList } from "@/components/tasks/tasks-list"
+import * as React from "react"
 
 export default function TasksPage() {
   return (
     <AppLayout>
       <div className="container mx-auto py-6">
-        <TasksList />
+        <React.Suspense fallback={<div>Loading tasks...</div>}>
+          <TasksList />
+        </React.Suspense>
       </div>
     </AppLayout>
   )
